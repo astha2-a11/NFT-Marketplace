7c570af72996ed56ae4ac88bdf6f80fb5bd0a519
astha2-a11 <patelastha512@gmail.com>
2025-06-20 12:31:19 +0530

    Create CryptoCovenant.sol

---
+++ CryptoCovenant.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract CryptoCovenant {
+    address public owner;
+    bool public isWillActive;
+    uint256 public unlockTime;
+    address public beneficiary;
+    string private assetDetails;
+
+    constructor(address _beneficiary, uint256 _unlockTime, string memory _assetDetails) {
+        owner = msg.sender;
+        beneficiary = _beneficiary;
+        unlockTime = _unlockTime;
+        assetDetails = _assetDetails;
+        isWillActive = true;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this");
+        _;
+    }
+
+    modifier onlyAfterUnlock() {
+        require(block.timestamp >= unlockTime, "Will is locked");
+        _;
+    }
+
+    function updateBeneficiary(address _newBeneficiary) external onlyOwner {
+        beneficiary = _newBeneficiary;
+    }
+
+    function getAssetDetails() external view onlyAfterUnlock returns (string memory) {
+        return assetDetails;
+    }
+
+    function revokeWill() external onlyOwner {
+        isWillActive = false;
+    }
+
+    function transferOwnership(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+    }
+}
